ec84152a32f8fd4546e0a389f36367a050ebffd3
Imam Faraz Aditya <imamaditya2211@gmail.com>
2023-05-17 13:24:29 +0700

    use search params on auth component & add links add and load expenses

--- app/routes/_app.expenses.tsx
+++ app/routes/_app.expenses.tsx
@@ -1,4 +1,5 @@
-import { Outlet } from "@remix-run/react";
+import { Link, Outlet } from "@remix-run/react";
+import { FaDownload, FaPlus } from "react-icons/fa";
 import ExpensesList from "~/components/expenses/ExpensesList";
 
 
@@ -29,6 +30,16 @@ export default function ExpensesPage() {
     <>
       <Outlet />
       <main>
+        <section id="expenses-actions">
+          <Link to="add">
+            <FaPlus />
+            <span>Add Expense</span>
+          </Link>
+          <a href="/expenses/raw">
+            <FaDownload />
+            <span>Load Raw Data</span>
+          </a>
+        </section>
         <ExpensesList expenses={DUMMY_EXPENSES} />
       </main>
     </>
