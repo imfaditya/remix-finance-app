13ae16518f377b0697553f20fb2f26720cdf62f9
Imam Faraz Aditya <imamaditya2211@gmail.com>
2023-05-22 21:11:08 +0700

    add addExpense function on server side

---
+++ app/data/expense.server.ts
@@ -0,0 +1,14 @@
+import { prisma } from "./database.server";
+
+export async function addExpense(expenseData: {title: string, amount: string, date: string}) {
+  try {
+    return await prisma.expense.create({data: {
+      title: expenseData.title,
+      amount: +expenseData.amount,
+      date: new Date(expenseData.date),
+    }})
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
