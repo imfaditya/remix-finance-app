5b24f75c9d5aa902016329d12db68b515a6dd91f
Imam Faraz Aditya <imamaditya2211@gmail.com>
2023-05-28 16:50:35 +0700

    add validation request method, add action delete

--- app/routes/_app.expenses.$id.tsx
+++ app/routes/_app.expenses.$id.tsx
@@ -1,8 +1,11 @@
 // import type { LoaderArgs } from "@remix-run/node";
+import { ActionArgs, redirect } from "@remix-run/node";
 import type { NavigateFunction } from "@remix-run/react";
 import { useNavigate } from "@remix-run/react";
 import ExpenseForm from "~/components/expenses/ExpenseForm";
 import Modal from "~/components/util/Modal";
+import { deleteExpense, updateExpense } from "~/data/expense.server";
+import { validateExpenseInput } from "~/data/validation.server";
 // import { getExpense } from "~/data/expense.server";
 
 export default function UpdateExpensesPage() {
@@ -22,3 +25,33 @@ export default function UpdateExpensesPage() {
 // export async function loader({ params }: LoaderArgs) {
 //   const expense = await getExpense(params.id || "");
 // }
+
+export async function action ({ params, request } : ActionArgs) {
+  const expenseId = params.id;
+
+  if(!expenseId) {
+    return redirect("/expenses");
+  }
+
+  if(request.method === 'DELETE') {
+    await deleteExpense(expenseId);
+  }
+
+  if(request.method === 'PATCH') {
+    const formData = await request.formData();
+    const { title, amount, date } = Object.fromEntries(formData);
+
+    if(typeof title !== "string" || typeof amount !== "string" || typeof date !== "string") {
+      return redirect("/expenses");
+    }
+
+    try {
+      validateExpenseInput({ title, amount, date });
+      await updateExpense(expenseId, { title, amount, date });
+    } catch (error) {
+      return error;
+    }
+  }
+
+  return redirect("/expenses");
+}
