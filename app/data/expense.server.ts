dcbebb4420679b3f0870478e374e169634e2a5bd
Imam Faraz Aditya <imamaditya2211@gmail.com>
2023-05-28 16:48:20 +0700

    add delete expense method

--- app/data/expense.server.ts
+++ app/data/expense.server.ts
@@ -31,3 +31,30 @@ export async function getExpense(id: string) {
     throw error;
   }
 }
+
+export async function updateExpense(id: string, expenseData: { title: string, amount: string, date: string }) {
+  try {
+    return await prisma.expense.update({
+      where: {id},
+      data: {
+        title: expenseData.title,
+        amount: +expenseData.amount,
+        date: new Date(expenseData.date),
+      }
+    })
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function deleteExpense(id: string) {
+  try {
+    return await prisma.expense.delete({
+      where: {id}
+    })
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
